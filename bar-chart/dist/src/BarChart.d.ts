d138a76d8de4b64fde94a493009e915e15602000
tiaant <tiaandt01@gmail.com>
2023-04-02 21:05:01 +0200

    removed dist from git ignore

---
+++ bar-chart/dist/src/BarChart.d.ts
@@ -0,0 +1,10 @@
+import { LitElement } from 'lit';
+export declare class BarChart extends LitElement {
+    static styles: import("lit").CSSResult;
+    test: string;
+    data: never[];
+    renderChart(): void;
+    firstUpdated(): void;
+    createRenderRoot(): this;
+    render(): import("lit-html").TemplateResult<1>;
+}
---
+++ bar-chart/dist/src/index.js
@@ -0,0 +1,2 @@
+export { BarChart } from './BarChart.js';
+//# sourceMappingURL=index.js.map
